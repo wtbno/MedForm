20725eca74de4225aaaceb3ac0f3f962df2cbfef
wtbno <bruno_links@hotmail.com>
2022-08-19 13:16:35 -0300

    first commit

---
+++ Cadastro.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Project
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Senha = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Email
+            // 
+            resources.ApplyResources(this.Email, "Email");
+            this.Email.Name = "Email";
+            this.Email.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Senha
+            // 
+            resources.ApplyResources(this.Senha, "Senha");
+            this.Senha.Name = "Senha";
+            // 
+            // Login
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Senha);
+            this.Controls.Add(this.Email);
+            this.Name = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox Senha;
+    }
+}
+
