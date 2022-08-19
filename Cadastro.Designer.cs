8c22a05d25a42297a015b911579a2f60efc4710d
wtbno <bruno_links@hotmail.com>
2022-08-19 16:07:35 -0300

    FEAT.: IMG LOGIN

--- Cadastro.Designer.cs
+++ Cadastro.Designer.cs
@@ -29,20 +29,16 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.Senha = new System.Windows.Forms.TextBox();
             this.enterBtn = new System.Windows.Forms.Button();
             this.newUser = new System.Windows.Forms.Button();
             this.Email = new System.Windows.Forms.TextBox();
-            this.User = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.User = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.User)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Senha
-            // 
-            resources.ApplyResources(this.Senha, "Senha");
-            this.Senha.Name = "Senha";
-            this.Senha.TextChanged += new System.EventHandler(this.Senha_TextChanged);
             // 
             // enterBtn
             // 
@@ -61,17 +57,11 @@
             // 
             // Email
             // 
+            this.Email.AcceptsTab = true;
             resources.ApplyResources(this.Email, "Email");
             this.Email.Name = "Email";
-            this.Email.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // User
-            // 
-            resources.ApplyResources(this.User, "User");
-            this.User.Image = global::Project.Properties.Resources.users;
-            this.User.Name = "User";
-            this.User.TabStop = false;
-            this.User.Click += new System.EventHandler(this.User_Click);
+            this.Email.ShortcutsEnabled = false;
+            this.Email.TextChanged += new System.EventHandler(this.textBox1);
             // 
             // label1
             // 
@@ -79,15 +69,41 @@
             this.label1.Name = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // passwordLabel
+            // 
+            resources.ApplyResources(this.passwordLabel, "passwordLabel");
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // emailLabel
+            // 
+            resources.ApplyResources(this.emailLabel, "emailLabel");
+            this.emailLabel.Name = "emailLabel";
+            // 
+            // User
+            // 
+            resources.ApplyResources(this.User, "User");
+            this.User.Image = global::Project.Properties.Resources.entrar;
+            this.User.Name = "User";
+            this.User.TabStop = false;
+            this.User.Click += new System.EventHandler(this.User_Click);
+            // 
             // Login
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.User);
             this.Controls.Add(this.newUser);
             this.Controls.Add(this.enterBtn);
-            this.Controls.Add(this.Senha);
             this.Controls.Add(this.Email);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Login";
@@ -99,12 +115,14 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox Senha;
         private System.Windows.Forms.Button enterBtn;
         private System.Windows.Forms.Button newUser;
         private System.Windows.Forms.TextBox Email;
         private System.Windows.Forms.PictureBox User;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label emailLabel;
     }
 }
 
